58acdb41c9b0b861bd0ab4391ee139d0ed97f3b8
Yoni19 <anthony.krief@hotmail.fr>
2020-10-22 18:44:16 +0200

    Updates

---
+++ precache-manifest.217433d7f787c73fb421be98d8fd4875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f08d82bc9d41c3117e19220f961e022",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07922fec13cc484ee743",
+    "url": "/static/css/main.92e6c59f.chunk.css"
+  },
+  {
+    "revision": "4b0879b4ec21d7394cb1",
+    "url": "/static/js/2.8e0bb561.chunk.js"
+  },
+  {
+    "revision": "47f6767b7e88d0db85618bec5367f1c7",
+    "url": "/static/js/2.8e0bb561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07922fec13cc484ee743",
+    "url": "/static/js/main.0a6cf438.chunk.js"
+  },
+  {
+    "revision": "3a8c3a9a4bcf11213d1b",
+    "url": "/static/js/runtime-main.cc7e03ee.js"
+  }
+]);
